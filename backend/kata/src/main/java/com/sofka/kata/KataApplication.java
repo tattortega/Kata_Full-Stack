31bd1a4a34d2da063122c7c7453a94ab0fa8f340
Ricardo Ortega <tattortega.28@gmail.com>
2022-06-16 16:38:49 -0500

    Estructura y configuracion del proyecto en spring boot

---
+++ backend/kata/src/main/java/com/sofka/kata/KataApplication.java
@@ -0,0 +1,20 @@
+package com.sofka.kata;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class KataApplication {
+
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataApplication.class, args);
+    }
+
+}
