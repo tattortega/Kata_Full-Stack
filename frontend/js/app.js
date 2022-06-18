4278f5887f7abb34356e4e9579e024a2590a6cfc
Ricardo Ortega <tattortega.28@gmail.com>
2022-06-17 19:47:31 -0500

    Agregando el evento y funcion para crear listas

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,26 +1,9 @@
 const api = "http://localhost:8080";
-let data = "";
 let res;
 let json;
-const newToDoList = document.getElementById("newTodoList");
-const createTodoList = document.getElementById("btnCreateToDoList")
-const form = document.querySelector("form");
+const $form = document.getElementById("formList");
 
 const container = document.getElementById("container")
-// const template = document.getElementById("containerToDoList").content;
-let toDoListCreated = document.getElementById("ToDoListCreated");
-let deleteList = document.getElementById("btnDeleteToDoList");
-let newTodo = document.getElementById("newTodo");
-let createTodo = document.getElementById("btnCreateToDo")
-let table = document.getElementById("listToDo")
-let idTodo = document.getElementById("id")
-let toDo = document.getElementById("toDo")
-let completed = document.getElementById("completed")
-let editTodo = document.getElementById("edit")
-let deleteToDo = document.getElementById("delete")
-const fragment = document.createDocumentFragment();
-let tbody = document.querySelector("tbody")
-let data1 = ""
 let todo = ""
 let todoList = ""
 
@@ -34,15 +17,15 @@ async function getData() {
             todo = ''
             toDoList.todos.forEach(toDo => {
                 todo += ` <tr>
-            <td class="id">${toDo.id}</td>
-            <td class="Tarea">${toDo.name}</td>
-            <td class="completado">
+            <td>${toDo.id}</td>
+            <td>${toDo.name}</td>
+            <td>
                 <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
                 <label class="form-check-label" for="flexSwitchCheckDefault"></label>
             </td>
-            <td class="opciones">
-                <button type="button" id="editar${toDo.id}" style="margin-right: 1rem" class="btn btn-primary">Editar</button>
-                <button class="eliminar btn btn-danger" type="button" id="eliminar${toDo.id}" >Eliminar</button>
+            <td>
+                <button class="editToDo btn btn-primary" type="button" id="edit${toDo.id}" style="margin-right: 1rem" >Editar</button>
+                <button class="deleteToDo btn btn-danger" type="button" id="delete${toDo.id}" >Eliminar</button>
             </td>
         </tr>`
             })
@@ -50,12 +33,12 @@ async function getData() {
     <div>
         <h2 id="nombre-lista" style="text-align: start">${toDoList.name}</h2>
         <spam class = "spamId" area hidden true>${toDoList.id}</spam>
-        <button class="EliminarTarea btn btn-outline-danger" type="submit" id="borrar${toDoList.id}" ">Eliminar</button>
+        <button class="deleteToDoList btn btn-outline-danger" type="submit" id="delete${toDoList.id}" ">Eliminar</button>
     </div> 
     <br>
     <div class="input-group mb-5">
-        <input class="form-control me-sm-2" style="width: 50%" type="text" id="inputTarea${toDoList.id}" placeholder="¿Que piensas hacer?">
-        <button class="agregarSubList btn btn-primary my-2 my-sm-0" type="submit" value="${toDoList.id}">Crear</button>
+        <input class="form-control me-sm-2" style="width: 50%" type="text" id="inputToDo${toDoList.id}" placeholder="¿Que piensas hacer?">
+        <button class="saveToDo btn btn-primary my-2 my-sm-0" type="submit" value="${toDoList.id}">Crear</button>
     </div>
     <table id="tabla" class="table mt-2 table-bordered" style="text-align: center">
         <thead>
@@ -83,5 +66,31 @@ async function getData() {
 
 document.addEventListener("DOMContentLoaded", getData());
 
+//Evento que escucha el click de crear nueva lista e invoca la funcion
+$form.addEventListener("submit", e => {
+    e.preventDefault()
+    createList(document.getElementById("newTodoList").value)
+})
 
+//Funcion para crear nueva lista
+async function createList(newList) {
+    res = await fetch(`${api}/todo-list`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: newList,
+        })
+    })
+        .then(response => response.json())
+        .then(() => {
+            location.reload()
+            getData()
+        })
+        .catch(err => {
+            let message = err.statusText || "Ocurrió un error";
+            container.insertAdjacentHTML("afterend", `<p style="text-align: center"><b>Error ${err.status}: ${message}</b></p>`);
+        })
+}
 
